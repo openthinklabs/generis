66864110fb09787c67d0684e83d8330472d415ca
Wildan Maulana <wildan.m@gmail.com>
2022-02-17 17:15:42 +0700

    add default config for persistencess

--- config/header/persistences.conf.php
+++ config/header/persistences.conf.php
@@ -13,3 +13,79 @@
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  * @see common_persistence_Manager
  */
+<?php
+/**
+ * Default config header created during install
+ */
+
+return new oat\generis\persistence\PersistenceManager(array(
+    'persistences' => array(
+        'default' => array(
+            'driver' => 'dbal',
+            'connection' => array(
+                'driver' => 'pdo_mysql',
+                'driverClass' => null,
+                'instance' => null,
+                'host' => 'localhost',
+                'dbname' => 'cbt_dbname',
+                'user' => 'cbt_user',
+                'password' => 'cbt_db_password',
+                'driverOptions' => array(
+                ),
+                'charset' => 'utf8'
+            )
+        ),
+        /** 
+        'default_kv' => array(
+            'driver' => 'common_persistence_SqlKvDriver',
+            'sqlPersistence' => 'default'
+        ),
+        **/
+        'default_kv' => array(
+            'driver' => 'phpredis',
+            'host' => '127.0.0.1',
+            'port' => 6379
+        ),
+        /**                                                
+        'serviceState' => array(
+            'driver' => 'phpfile'
+        ),
+        */
+        'serviceState' => array(
+            'driver' => 'phpredis',
+            'host' => '127.0.0.1',
+            'port' => 6379
+        ),        
+        'deliveryExecution' => array(
+            'driver' => 'phpredis',
+            'host' => '127.0.0.1',
+            'port' => 6379
+        ),        
+        'cache' => array(
+            'driver' => 'phpfile'
+        ),
+        'uriProvider' => array(
+            'driver' => 'phpredis',
+            'host' => '127.0.0.1',
+            'port' => 6379
+        ),
+        'session' => array(
+            'driver' => 'phpredis',
+            'host' => '127.0.0.1',
+            'port' => 6379
+        ),
+        'authKeyValue' => array(
+            'driver' => 'phpredis',
+            'host' => '127.0.0.1',
+            'port' => 6379
+        ),
+        'keyValueResult' => array(
+            'driver' => 'phpredis',
+            'host' => '127.0.0.1',
+            'port' => 6379
+        ),
+        'maintenance' => array(
+            'driver' => 'phpfile'
+        )        
+    )
+));
